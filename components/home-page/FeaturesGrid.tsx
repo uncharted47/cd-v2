79ccb9d7da8022cc37468b4d0c28a6be1e125fee
uncharted47 <mehdilyz5@gmail.com>
2025-09-12 16:50:20 +0100

    test

--- components/home-page/FeaturesGrid.tsx
+++ components/home-page/FeaturesGrid.tsx
@@ -49,7 +49,7 @@ const FeaturesGrid = () => {
       >
         Clé Blanche Chapitre II
       </h2>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 container mx-auto">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 container mx-auto p-4">
         {features.map((feature, i) => (
           <div
             key={i}
